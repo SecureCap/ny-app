7afc52afbe870851fba02ff214e7d472a49298bf
Stephen Ridings <SRidings777@aol.com>
2018-08-03 12:34:41 -0500

    onClicks for each class

--- API/NYtimesJS.js
+++ API/NYtimesJS.js
@@ -1,1 +1,25 @@
+    $("#search").on("click", function() 
+    {
 
+      
+    });
+    $("#record").on("click", function() 
+    {
+
+      
+    });
+    $("#startYear").on("click", function() 
+    {
+
+      
+    });
+    $("#searchBtn").on("click", function() 
+    {
+
+      
+    });
+    $("#articles").on("click", function() 
+    {
+
+      
+    });
